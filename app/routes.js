a3bd939ac73183420e2c33843b3777c014a95742
Yameen Yasin <yamin.kmr@gmail.com>
2016-06-11 21:41:39 +0530

    Initial Commit
    Initial Commit

---
+++ app/routes.js
@@ -0,0 +1,41 @@
+// grab the nerd model we just created
+var User = require('./models/user');
+
+module.exports = function (app) {
+
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+
+    // sample api route
+    app.get('/api/user', function (req, res) {
+
+        var user = new User({
+            email: "test.kmr@gmail.com",
+            password: "test"
+        });
+        user.save(function (err, fluffy) {
+            if (err) return console.error(err);
+            console.log("User save success");
+            res.send("Yaay !! Successs ... ");
+        });
+    });
+
+    // route to handle creating goes here (app.post)
+    // route to handle delete goes here (app.delete)
+
+    app.get('/partials/:filename', function (req, res) {
+        console.log(req.params.filename);
+        var filename = req.params.filename;
+        if (!filename) return; // might want to change this
+        res.sendFile(filename, {
+                root: './public/views/partials'
+        });
+    });
+    app.use(function (req, res) {
+            res.sendFile('index.html', {
+                root: './public/views'
+            });
+    });
+
+};
---
+++ config/db.js
@@ -0,0 +1,4 @@
+// config/db.js
+module.exports = {
+    url: 'mongodb://yameenysain:yameen123@ds025583.mlab.com:25583/yameen-db'
+}
